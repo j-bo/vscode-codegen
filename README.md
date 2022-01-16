3ba22b9685d399616e278c74d28918c7e9385b37
j-bo <j.boucheteil@gmail.com>
2022-01-16 14:10:29 +0100

    Ready for 1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # id3codegen 
 
 Little extension for the codegen magic tool !
+
+- Automatically check for codegen updates
+- Check if current project is up-to-date
+- Run codegen with a clic on the status bar
--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,53 @@
 import * as vscode from 'vscode';
 import * as cp from "child_process";
+import { openStdin } from 'process';
 
 let currentCodegenVersion: string;
 let lastCodegenVersion: string;
 let projectCodegenVersion: string;
 let codegenStatusBarItem: vscode.StatusBarItem;
+let pipCmd: string;
+let newVersion: boolean;
+let installed: boolean;
+let timer: NodeJS.Timer;
 
 const execShell = (cmd: string) =>
     new Promise<string>((resolve, reject) => {
         cp.exec(cmd, (err, out) => {
             if (err) {
-                return reject(err);
+                return resolve(err.message);
             }
             return resolve(out);
         });
 });
 
 async function getLastVersion() {
-	let outDated = (await execShell('pip list --outdated ')).match(/.*id3codegen.*/g);
-	if(outDated) {
-		let lastVersion = outDated[0].match(/\d.\d.\d/g);
-		if(lastVersion) {
-			return lastVersion[1];
+	let cmd_ret = await execShell('pip list --outdated ');
+	if(!cmd_ret.includes('failed')) {
+		let outDated = cmd_ret.match(/.*id3codegen.*/g);
+		if(outDated) {
+			let lastVersion = outDated[0].match(/\d.\d.\d/g);
+			if(lastVersion) {
+				return lastVersion[1];
+			}
 		}
 	}
-	return currentCodegenVersion;
+	return 'N/A';
 }
 
 async function getCurrentVersion() {
-	let localVersion = (await execShell('pip show id3codegen')).match(/\d.\d.\d/g);
-	if(localVersion) {
-		codegenStatusBarItem.text = 'codegen v' + localVersion[0];
-		return localVersion[0];
+	let cmd_ret = await execShell('pip show id3codegen');
+	if(cmd_ret.includes('failed')) {
+		vscode.window.showWarningMessage('Codegen is not installed.', ...['Install']).then(selection => {
+			installCodegen();
+		});
+	} else {
+		let localVersion = cmd_ret.match(/\d.\d.\d/g);
+		if(localVersion) {
+			installed = true;
+			codegenStatusBarItem.text = 'codegen v' + localVersion[0];
+			return localVersion[0];
+		}
 	}
 	return "Not installed";
 }
@@ -46,14 +62,36 @@ async function getProjectVersion() {
 			return projectVersion[0];
 		}
 	}
-	return "N/A";
+	return 'N/A';
+}
+
+async function installCodegen() {
+	let cmd_ret = await execShell('pip install id3codegen');
+	if(!cmd_ret.includes('failed')) {
+		currentCodegenVersion = await getCurrentVersion();
+		vscode.window.showInformationMessage('Successfully updated codegen to v' + currentCodegenVersion + '!');
+		installed = true;
+		setUpdateTimer();
+		checkProjectUpToDate();
+	} else {
+		vscode.window.showErrorMessage('Error during codegen install. Please run "pip install id3codegen" manually.');
+	}
 }
 
 async function updateCodegen() {
-	vscode.window.showInformationMessage('Updating codegen to v' + lastCodegenVersion + '...');
-	await execShell('pip install -U id3codegen');
-	currentCodegenVersion = await getCurrentVersion();
-	vscode.window.showInformationMessage('Successfully updated codegen to v' + currentCodegenVersion + '!');
+	let cmd_ret = await execShell('pip install -U id3codegen');
+	if(!cmd_ret.includes('failed')) {
+		currentCodegenVersion = await getCurrentVersion();
+		vscode.window.showInformationMessage('Successfully updated codegen to v' + currentCodegenVersion + '!', ...['See Changelog']).then(selection => {
+			if(selection === 'See Changelog') {
+				vscode.env.openExternal(vscode.Uri.parse('https://gitlab.srv.int.id3.eu/biometrics/algos/development/codegen/-/blob/' + currentCodegenVersion + '/CHANGELOG.md'));
+			}
+		});
+		newVersion = false;
+		checkProjectUpToDate();
+	} else {
+		vscode.window.showErrorMessage('Error during codegen update. Please run "pip install -U id3codegen" manually.');
+	}
 }
 
 async function runCodegen() {
@@ -65,33 +103,72 @@ async function runCodegen() {
 	}
 }
 
-export async function activate({ subscriptions }: vscode.ExtensionContext) {
-	codegenStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-
-	currentCodegenVersion = await getCurrentVersion();
-	codegenStatusBarItem.command = 'extension.runCodegen';
-	codegenStatusBarItem.text = 'codegen v' + currentCodegenVersion;
-	subscriptions.push(codegenStatusBarItem);
-	codegenStatusBarItem.show();
-
+async function checkForUpdate() {
 	lastCodegenVersion = await getLastVersion();
-	if(lastCodegenVersion !== currentCodegenVersion) {
+	if(lastCodegenVersion !== currentCodegenVersion && lastCodegenVersion !== 'N/A') {
+		newVersion = true;
 		vscode.window.showWarningMessage('A new version of codegen is available : v'+lastCodegenVersion, ...['Update']).then(selection => {
-			updateCodegen();
+			if(selection === 'Update') {
+				updateCodegen();
+			}
 		});
 	}
+}
 
+async function checkProjectUpToDate() {
 	projectCodegenVersion = await getProjectVersion();
 	if(projectCodegenVersion !== "N/A" && projectCodegenVersion !== currentCodegenVersion) {
 		vscode.window.showWarningMessage('This project present an out-dated codegen API : v'+projectCodegenVersion, ...['Run codegen']).then(selection => {
-			runCodegen();
+			if(selection === 'Run codegen') {
+				runCodegen();
+			}
 		});
 	}
+}
 
+function getPipCmd() {
+	const os = require('os');
+	let osName = os.platform();
+	if(osName === 'linux' || osName === 'darwin')  {
+		return 'pip3';
+	} else {
+		return 'pip';
+	}
+}
+
+async function checkVersions() {
+	await checkForUpdate();
+	if(! newVersion) {
+		checkProjectUpToDate();
+	}
+}
+
+function setUpdateTimer() {
+	timer = setInterval(() => checkVersions(),1000*60*10); // check every 10min
+}
+
+export async function activate({ subscriptions }: vscode.ExtensionContext) {
+	installed = false;
+	pipCmd = getPipCmd();
+	codegenStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	codegenStatusBarItem.command = 'extension.runCodegen';
+	subscriptions.push(codegenStatusBarItem);
+	
 	let disposableRunCodegen = vscode.commands.registerCommand('extension.runCodegen', async () => {
 		runCodegen();
 	});
 	subscriptions.push(disposableRunCodegen);
+
+	currentCodegenVersion = await getCurrentVersion();
+	if(installed) {
+		codegenStatusBarItem.text = 'codegen v' + currentCodegenVersion;
+		codegenStatusBarItem.show();
+		checkVersions();
+		setUpdateTimer();
+	} else {
+		codegenStatusBarItem.text = 'codegen not installed';
+		codegenStatusBarItem.show();
+	}
 }
 
 export function deactivate() {}
